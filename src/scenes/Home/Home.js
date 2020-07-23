a7d2bef26df64525a28ec1131216c94e22b31237
Stiven Tovar <stivent367@gmail.com>
2020-07-23 04:26:45 -0500

    Change letters functiionality

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 function Home() {
+  const [word, setWord] = useState('rosas');
+  const [showWord, setShowWord] = useState(word);
+  const [currentWord, setCurrentWord] = useState(word.split(''));
+
+  const abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+  const changeLetter = (direction, positionWord, positionLetter) => {
+    let countLetter = positionLetter
+    let newWord = currentWord
+    console.log(direction, positionWord, positionLetter)
+    console.log(newWord)
+    console.log('====================00')
+
+    if (direction !== 'top') {
+      console.log('resta')
+      countLetter--
+      if (countLetter < 0)
+        countLetter = abc.length - 1
+    } else {
+      console.log('suma')
+      countLetter++
+      if (countLetter > abc.length - 1)
+        countLetter = 0
+    }
+
+    console.log(countLetter, 'cuent')
+    newWord[positionWord] = abc[countLetter]
+    console.log(newWord)
+    setCurrentWord(newWord)
+    setShowWord(currentWord.join(''))
+  }
+
+  const reset = () => {
+    setCurrentWord(word.split(''))
+    setShowWord(word)
+  }
+
   return (
     <div className="home">
       <div className="header">PhoneWare</div>
+      <div className="header">{showWord}</div>
+      <button type="button" onClick={reset}>Reiniciar</button>
       <div className="content">
-        contenido
+        <div className="roll-word">
+          {[...Array(word.length)].map((e, i) =>
+            <div className="roll-letter" key={i}>
+              {
+                abc.map((item, j) =>
+                  currentWord[i] === item &&
+                  <div key={j}>
+                    <button type="button" className="arrow arrow-top" onClick={() => changeLetter('top', i, j)}>▲</button>
+                    <button type="button" className="arrow arrow-bottom" onClick={() => changeLetter('bottom', i, j)}>▼</button>
+                    <span>{item}</span>
+                  </div>
+                )}
+            </div>
 
+          )}
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 }
