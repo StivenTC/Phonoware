0568cefbb9fff33429d45c53a982a73120459132
Stiven Tovar <stivent367@gmail.com>
2020-07-23 08:21:30 -0500

    Added main functionality

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,62 +1,123 @@
 import React, { useState } from 'react';
 function Home() {
-  const [word, setWord] = useState('rosas');
-  const [showWord, setShowWord] = useState(word);
-  const [currentWord, setCurrentWord] = useState(word.split(''));
+  const [initWord, setInitWord] = useState('rosas'); //palabra inicial
+  const [showWord, setShowWord] = useState(initWord); //palabra mostrada para render
+  const [currentWord, setCurrentWord] = useState(initWord.split('')); //palabra cambiando
+  const [listWords, setListWords] = useState([initWord]) //LIStado palabras completadas  
 
+  const [lifes, setLifes] = useState(3);
+  const [isLoading, setLoading] = useState(false);
+
+  const [lastWordPosition, setLastWordPosition] = useState(-1);
   const abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
   const changeLetter = (direction, positionWord, positionLetter) => {
     let countLetter = positionLetter
     let newWord = currentWord
-    console.log(direction, positionWord, positionLetter)
-    console.log(newWord)
-    console.log('====================00')
+    let originalLetter = initWord.split('')[lastWordPosition]
+
+    if (lastWordPosition !== positionWord) {
+      newWord[lastWordPosition] = originalLetter
+      setLastWordPosition(positionWord)
+    }
 
     if (direction !== 'top') {
-      console.log('resta')
       countLetter--
       if (countLetter < 0)
         countLetter = abc.length - 1
     } else {
-      console.log('suma')
       countLetter++
       if (countLetter > abc.length - 1)
         countLetter = 0
     }
 
-    console.log(countLetter, 'cuent')
+    setLastWordPosition(positionWord)
     newWord[positionWord] = abc[countLetter]
-    console.log(newWord)
     setCurrentWord(newWord)
     setShowWord(currentWord.join(''))
   }
 
-  const reset = () => {
-    setCurrentWord(word.split(''))
-    setShowWord(word)
+  const resetWord = () => {
+    setCurrentWord(initWord.split(''))
+    setShowWord(initWord)
+  }
+
+  async function verifyWordRae() {
+    console.log(showWord)
+    const response = await fetch('https://cors-anywhere.herokuapp.com/https://dle.rae.es/' + showWord);
+    return await response.text();
+  }
+
+  async function checkWord() {
+    setLoading(true)
+    let arrayWords = listWords
+
+    if (!listWords.includes(showWord)) {
+
+      let response = await verifyWordRae()
+      response = response.substring(response.search('<title>'), response.search('</title>'))
+
+      if (!response.includes('| Edición del Tricentenario |')) {
+        arrayWords.push(showWord)
+        setListWords(arrayWords)
+        setInitWord(showWord)
+      } else {
+        setLifes(lifes - 1)
+        if (lifes === 0) {
+          resetWord()
+        }
+        setInitWord(initWord)
+      }
+    } else {
+      resetWord()
+      if (lifes !== 1) {
+        setLifes(lifes - 1)
+      }
+    }
+    setLoading(false)
   }
 
   return (
     <div className="home">
-      <div className="header">PhoneWare</div>
-      <div className="header">{showWord}</div>
-      <button type="button" onClick={reset}>Reiniciar</button>
+      <div className="header">
+        <h1>PhoneWare</h1>
+        <div className="lifes">{
+          [...Array(lifes)].map((item, i) =>
+            <span>♥</span>
+          )}
+        </div>
+        <div className="count-words">{listWords.length - 1}</div>
+        <div className="current-word">{showWord}</div>
+      </div>
+
       <div className="content">
         <div className="roll-word">
-          {[...Array(word.length)].map((e, i) =>
+          {[...Array(initWord.length)].map((e, i) =>
             <div className="roll-letter" key={i}>
               {
                 abc.map((item, j) =>
                   currentWord[i] === item &&
                   <div key={j}>
-                    <button type="button" className="arrow arrow-top" onClick={() => changeLetter('top', i, j)}>▲</button>
-                    <button type="button" className="arrow arrow-bottom" onClick={() => changeLetter('bottom', i, j)}>▼</button>
+                    <button type="button" disabled={isLoading} className="arrow arrow-top" onClick={() => changeLetter('top', i, j)}>▲</button>
+                    <button type="button" disabled={isLoading} className="arrow arrow-bottom" onClick={() => changeLetter('bottom', i, j)}>▼</button>
                     <span>{item}</span>
                   </div>
                 )}
             </div>
 
+          )}
+        </div>
+        <div className="check-content">
+          <div className="check-word">
+            {isLoading ?
+              <p>Verificando...</p> :
+              <button type="button" disabled={showWord === initWord || isLoading} onClick={checkWord}>Verificar</button>
+            }
+          </div>
+        </div>
+        <div className="list-word">
+          {listWords.map((item, i) =>
+            <p key={i}>{item}</p>
           )}
         </div>
       </div>
